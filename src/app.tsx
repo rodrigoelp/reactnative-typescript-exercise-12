0bacae56cd17cdbafa7e4144f55a4973e2a73991
Rodrigo Landaeta <rlandaeta@cochlear.com>
2017-12-02 23:03:10 +1100

    Project transfer into its own repo

---
+++ src/app.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+import { AppRegistry, View, Text } from "react-native";
+import { AppShell } from "./screens";
+
+AppRegistry.registerComponent("tabnav", () => AppShell);
